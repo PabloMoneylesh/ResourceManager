c1b6c7086d957c34ed59c317156d011f38e5eae0
PBezdienezhnykh <pablomoneylesh@gmail.com>
2016-10-08 16:16:56 +0200

    authorization test

--- src/test/java/com/hid/resourceManager/controllers/AuthenticationTest.java
+++ src/test/java/com/hid/resourceManager/controllers/AuthenticationTest.java
@@ -4,6 +4,7 @@ import com.hid.resourceManager.ResorceManagerApplication;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,42 +14,62 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import javax.servlet.Filter;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ResorceManagerApplication.class)
 @WebAppConfiguration
 public class AuthenticationTest {
 
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private Filter springSecurityFilterChain;
+
 
     private MockMvc mockMvc;
 
     @Before
     public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new Authentication())
-                //.setHandlerExceptionResolvers(exceptionResolver())
-                //.setValidator(validator())
-                //.setViewResolvers(viewResolver())
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .addFilters(springSecurityFilterChain)
                 .build();
     }
 
 
     @Test
-    public void home() throws Exception {
+    public void nonAuthorizedShouldByRedirectedToLogin() throws Exception {
 
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/");
 
         ResultActions resultActions = mockMvc.perform(requestBuilder);
-        resultActions.andExpect(status().isOk());
-
-
         MockHttpServletResponse response = resultActions.andReturn().getResponse();
+        System.out.println("Resul:");
+        System.out.println(response.getContentAsString());
+        System.out.println(response.getRedirectedUrl());
 
-        resultActions.andExpect(forwardedUrl("home"));
+        resultActions.andExpect(status().is(302));
+        resultActions.andExpect(redirectedUrlPattern("**/login"));
+
+    }
+
+    @Test
+    public void authorizedShouldByRedirectedToHome() throws Exception {
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/").with(user("user"));
+
+        ResultActions resultActions = mockMvc.perform(requestBuilder);
 
 
+        resultActions.andExpect(status().is(200));
+        resultActions.andExpect(redirectedUrl(null));
 
     }
 
