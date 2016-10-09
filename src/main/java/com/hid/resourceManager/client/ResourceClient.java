699737bd341a5d734d40d11a9925fed1778d12c5
PBezdienezhnykh <pablomoneylesh@gmail.com>
2016-10-09 10:06:25 +0200

    added client example for downloading resources

---
+++ src/main/java/com/hid/resourceManager/client/ResourceClient.java
@@ -0,0 +1,19 @@
+package com.hid.resourceManager.client;
+
+/**
+ * Created by comp on 09.10.2016.
+ */
+public class ResourceClient {
+
+    public void downloadResource(String resKey, String resClass) {
+
+    }
+
+    private String getSesionId() {
+        return null;
+    }
+
+    private void authenticate() {
+
+    }
+}
---
+++ src/test/java/com/hid/resourceManager/client/ResourceClientTest.java
@@ -0,0 +1,14 @@
+package com.hid.resourceManager.client;
+
+import org.junit.Test;
+
+/**
+ * Created by comp on 09.10.2016.
+ */
+public class ResourceClientTest {
+    @Test
+    public void downloadResource() throws Exception {
+
+    }
+
+}
